3397cd48ca0ae81f54d85b6442072be412c66608
Shevchenko Andrey <demorest49de@gmail.com>
2024-03-25 12:13:30 +0300

    Changes .gitignore 200.svg 400.svg 404.svg 500.svg Affair.module.css Affair.tsx Affairs.module.css Affairs.tsx App.module.css App.test.tsx App.tsx avatar.png burger.svg checked.png checked.svg chevron-down.svg Clock.module.css Clock.tsx closeOutline.svg deleteAffair.test.tsx editIcon.svg error.svg Error404.module.css Error404.tsx favicon.ico filterAffairs.test.tsx FriendMessage.module.css FriendMessage.tsx Greeting.module.css Greeting.tsx GreetingContainer.tsx Header.module.css Header.tsx homeWorkReducer.test.tsx homeWorkReducer.ts HW1.tsx HW2.tsx HW3.tsx HW4.tsx HW5.tsx HW6.module.css HW6.tsx HW7.module.css HW7.tsx HW8.module.css HW8.tsx HW9.tsx HW10.tsx HW11.module.css HW11.tsx HW12.module.css HW12.tsx HW13.module.css HW13.tsx HW14.module.css HW14.tsx HW15.module.css HW15.tsx index.css index.html index.tsx Junior.tsx JuniorPlus.tsx Layout.tsx Loader.module.css Loader.tsx loadingReducer.ts localStorage.ts logo192.png logo512.png manifest.json Message.module.css Message.tsx MessageSender.module.css MessageSender.tsx package.json Pages.tsx PreJunior.tsx pureAddUser.test.tsx pureAddUserCallback.test.tsx pureChange.test.tsx pureOnBlur.test.tsx pureOnEnter.test.tsx react-app-env.d.ts README.md reportWebVitals.ts robots.txt setupTests.ts Sidebar.module.css Sidebar.tsx Stand.module.css Stand.tsx store.ts SuperButton.module.css SuperButton.tsx SuperCheckbox.module.css SuperCheckbox.tsx SuperDebouncedInput.tsx SuperEditableSpan.module.css SuperEditableSpan.tsx SuperInputText.module.css SuperInputText.tsx SuperPagination.module.css SuperPagination.tsx SuperRadio.module.css SuperRadio.tsx SuperRange.tsx SuperSelect.module.css SuperSelect.tsx SuperSort.tsx themeReducer.ts tsconfig.json User.tsx yarn.lock

---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,71 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
+import Greeting from './Greeting'
+import {UserType} from './HW3'
+
+type GreetingContainerPropsType = {
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
+}
+
+export const pureAddUser = (name: string, setError: any, setName: any, addUserCallback: any) => {
+    if (!name) {
+        setError('error!')
+    } else {
+        addUserCallback()
+    }
+    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+}
+
+export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+}
+
+export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<any>('') // need to fix any
+    const [error, setError] = useState<any>('') // need to fix any
+
+    const setNameCallback = (e: any) => { // need to fix any
+        setName('some name') // need to fix
+
+        error && setError('')
+    }
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: any) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = 0 // need to fix
+    const lastUserName = 'some name' // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
