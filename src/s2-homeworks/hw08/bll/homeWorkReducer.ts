cf85a0902b9d12c5ca002e6ff6c9d31434f26530
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-28 09:59:10 +0300

    Changes homeWorkReducer.ts HW8.module.css Junior.tsx User.tsx

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const newState = [...state]
+            return action.payload === 'up' ? newState.sort((a, b) => a.name.localeCompare(b.name)) : newState.sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(user => user.age >= 18)
         }
         default:
             return state
