9e900987671aae2249e6482eb69fcdb05b85d9be
Shevchenko Andrey <demorest49de@gmail.com>
2024-05-22 15:04:17 +0300

    Changes SuperRange.tsx

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+    console.log(' props: ', props);
     return (
         <Slider
-            value={props.value}
-            onChange={props.onChange}
-
-
             sx={{ // стили для слайдера // пишет студент
-                width: '350px'
+                color: '#00CC22',
+                width: '350px',
+                '& .MuiSlider-rail.css-14pt78w-MuiSlider-rail': {
+                    color: '#8B8B8B',
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
